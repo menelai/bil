6b0dc36ac46876883f9f79532741d3b40eab547f
menelai <kovalenko@gmail.com>
2016-01-26 17:50:42 +0300

    4

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@
 
 (function($){
     $(function() {
-        $('.datepicker').datepicker();
+        $('head').append('<style type="text/css" id="datepickerstyle"></style>');
+        $('.datepicker').datepicker({
+            beforeShow: function(){
+
+                $("#datepickerstyle").html('.ui-datepicker {width:'+($(this).outerWidth()*2+2)+'px}');
+                //console.log($(this).outerWidth()*2+'px');
+            }
+        });
     });
 })(jQuery);
